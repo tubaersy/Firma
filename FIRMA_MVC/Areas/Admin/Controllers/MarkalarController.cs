d5c29770675aee538e8665760dd709b6515f4d67
tuba ersoy <tuba_ersy@hotmail.com>
2019-09-11 16:06:40 +0300

    urunler commit

---
+++ FIRMA_MVC/Areas/Admin/Controllers/MarkalarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc;
+using FIRMA_MVC.Models;
+
+namespace FIRMA_MVC.Areas.Admin.Controllers
+{
+    public class MarkalarController : Controller
+    {
+        FIRMAMODEL db = new FIRMAMODEL();
+        // GET: Admin/Markalar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FIRMA_MVC/Areas/Admin/Controllers/UrunlerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FIRMA_MVC.Models;
+
+
+namespace FIRMA_MVC.Areas.Admin.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        FIRMAMODEL db = new FIRMAMODEL();
+        // GET: Admin/Urunler
+        public ActionResult Index()
+        {
+            List<URUN> liste = db.URUNs.ToList();
+            return View(liste);
+        }
+        public ActionResult Delete(int? id)
+        {
+           
+            if (id != null)
+            {
+                URUN u = db.URUNs.Find(id);
+                if (u != null)
+                {
+                    db.URUNs.Remove(u);
+                    db.SaveChanges();
+                }
+            }
+            
+
+            // var liste = db.URUNs.ToList();
+            // return View("Index", liste);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create(int? id)
+        {
+            URUN urun = new URUN();
+            if (id != null) // güncelleme yapılacak
+            {
+                urun = db.URUNs.Find(id);
+                if (urun == null)
+                {
+                    urun = new URUN();
+                }
+            }
+
+            ViewData["kategori"] = db.KATEGORIs.ToList();
+            ViewBag.marka = db.MARKAs.ToList();
+
+            return View(urun);  // model binding yapılıyor
+
+        }
+        [HttpPost]
+        public ActionResult Create(URUN urun)
+        {
+            return View();
+        }
+        public ActionResult Search(string ara)
+        {
+            return View();
+        }
+    }
+}
